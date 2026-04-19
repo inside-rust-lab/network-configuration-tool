a4103225ba92f27c302ad9fe2e61f95980fa27db
eddie <eddie.eby@gmail.com>
2026-04-19 02:42:04 -0600

    add logging to device.py

--- device.py
+++ device.py
@@ -1,3 +1,5 @@
+import logging
+import os
 from netmiko import ConnectHandler
 from netmiko import NetmikoTimeoutException, NetmikoAuthenticationException
 
@@ -11,9 +13,33 @@ class NetworkDevice:
         self.password = None
         self.secret = None
         self.netconnect = None
+        self.logger = logging.getLogger(f"{__name__}.{hostname}")
+        self.logger.setLevel(logging.DEBUG)
+
+        if not self.logger.handlers:
+            directory = "logs"
+            if not os.path.isdir(directory):
+                os.makedirs(directory)
+            
+            file_hanlder = logging.FileHandler(f"logs/{hostname}.log")
+            file_hanlder.setLevel(logging.DEBUG)
+            formatter = logging.Formatter(
+                "%(asctime)s - %(levelname)s - %(message)s"
+            )
+            file_hanlder.setFormatter(formatter)
+
+            console_handler = logging.StreamHandler()
+            console_handler.setLevel(logging.INFO)
+            console_formatter = logging.Formatter(
+                "%(levelname)s - %(message)s"
+            )
+            console_handler.setFormatter(console_formatter)
+
+            self.logger.addHandler(file_hanlder)
+            self.logger.addHandler(console_handler)
 
     def connect(self):
-        print(f"Attempting to connect to {self.hostname}...")
+        self.logger.info(f"Attempting to connect to {self.hostname}")
         connection_established = False
         connection_attempts = 0
         max_retries = 2
@@ -27,17 +53,17 @@ class NetworkDevice:
                     password=self.password,
                     secret=self.secret
                 )
-                print(f"Successfully connected to {self.hostname}")
+                self.logger.info(f"Successfully connected to {self.hostname}")
                 connection_established = True
                 return connection_established
             except NetmikoAuthenticationException:
-                print("Invalid credentials")
+                self.logger.info("Invalid credentials")
                 return connection_established
             except NetmikoTimeoutException:
-                print(f"Unable to connect to {self.host}")
+                self.logger.info(f"Unable to connect to {self.host}")
                 connection_attempts += 1
             if connection_attempts >= max_retries:
-                print(f"Unable to connect to {self.host} after {max_retries} attempts")
+                self.logger.error(f"Unable to connect to {self.host} after {max_retries} attempts")
 
     def get_config(self):
         device_commands = {
@@ -56,10 +82,11 @@ class NetworkDevice:
                 if self.device_type == "adtran_os" or self.device_type == "cisco_ios":
                     self.net_connect.enable()
                 get_config_cmd = device_commands[self.device_type]["get_config"]
+                self.logger.debug(f"Sending: {get_config_cmd}")
                 output = self.net_connect.send_command(get_config_cmd)
-                return output              
-            except:
-                print(f"Unable to retrieve config file from {self.hostname}")
+                return output # returns the output to commandmanager.py so it can be saved to file           
+            except Exception as error:
+                self.logger.error(f"Error with config retreival from {self.hostname}: {error}")
                 return None
     
     def send_commands(self, device_commands):
@@ -71,23 +98,25 @@ class NetworkDevice:
                     self.net_connect.enable()
                 for command in command_list:
                     try:
+                        self.logger.debug(f"Sending: {command}")
                         output = self.net_connect.send_command(command)
                         commands_output.append(output)
-                        print(output)
+                        self.logger.info(f"Output: {output}")
                     except Exception as error:
-                        print(f"Error: {error}")
+                        self.logger.error(f"Error: {error}")
                         commands_output.append(error)
             if command_type == "configuration":
                 try:
+                    self.logger.debug(f"Sending: {command_list}")
                     output = self.net_connect.send_config_set(command_list)
                     commands_output.append(output)
-                    print(output)
+                    self.logger.info(f"Output: {output}")
                 except Exception as error:
-                    print(f"Error: {error}")
+                    self.logger.error(f"Error: {error}")
                     commands_output.append(error)
 
     def disconnect(self):
-        print(f"Disconnecting from {self.hostname}...")
+        self.logger.info(f"Disconnecting from {self.hostname}...")
         self.net_connect.disconnect()
 '''
 device.py functionality
